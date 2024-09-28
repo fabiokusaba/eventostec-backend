97eea5fcc06ca798a5b8e98e86f791d5bbd892f6
Fabio Kusaba <fabinhokb44@gmail.com>
2024-09-28 19:55:32 -0300

    feat: add consultCoupons

--- src/main/java/com/eventostec/api/repositories/CouponRepository.java
+++ src/main/java/com/eventostec/api/repositories/CouponRepository.java
@@ -3,7 +3,12 @@ package com.eventostec.api.repositories;
 import com.eventostec.api.domain.coupon.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 public interface CouponRepository extends JpaRepository<Coupon, UUID> {
+    // Declarando um novo metodo no nosso repository para fazer a busca por eventId e Valid, ou seja, todos os cupons
+    // que ainda estão válidos.
+    List<Coupon> findByEventIdAndValidAfter(UUID eventId, Date validAfter);
 }
