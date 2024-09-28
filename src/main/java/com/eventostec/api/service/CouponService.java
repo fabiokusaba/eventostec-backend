ec984431f10bd791c50a1e887dbfca949000becc
Fabio Kusaba <fabinhokb44@gmail.com>
2024-09-28 17:50:09 -0300

    feat: add coupons to event

---
+++ src/main/java/com/eventostec/api/service/CouponService.java
@@ -0,0 +1,38 @@
+package com.eventostec.api.service;
+
+import com.eventostec.api.domain.coupon.Coupon;
+import com.eventostec.api.domain.coupon.CouponRequestDTO;
+import com.eventostec.api.domain.event.Event;
+import com.eventostec.api.repositories.CouponRepository;
+import com.eventostec.api.repositories.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Service
+public class CouponService {
+
+    @Autowired
+    private CouponRepository couponRepository;
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    public Coupon addCouponToEvent(UUID eventId, CouponRequestDTO couponData) {
+        // Primeiro ele vai recuperar o evento através do id.
+        Event event = eventRepository.findById(eventId)
+                .orElseThrow(() -> new IllegalArgumentException("Event not found"));
+
+        // Encontrando esse evento ele faz o cadastro aqui do cupom.
+        Coupon coupon = new Coupon();
+        coupon.setCode(couponData.code());
+        coupon.setDiscount(couponData.discount());
+        coupon.setValid(new Date(couponData.valid()));
+        coupon.setEvent(event);
+
+        // Salvando o coupon no banco de dados.
+        return couponRepository.save(coupon);
+    }
+}
